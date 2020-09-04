40f563123d3df9e45b360e3d2454b75227803769
Bjarne Helland <bh@profitbase.no>
2020-09-04 10:03:46 +0200

    auth

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { useAuth } from '@lib/auth'
+
+export default function Home() {
+  const auth = useAuth()
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        {auth.user?.name}
+        <button onClick={() => auth.signinWithGoogle()}>Google</button>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
